97d3a3fc4006a83557979e178997dc7fece1b59c
MaximTkachev <69761681+MaximTkachev@users.noreply.github.com>
2023-01-01 13:37:56 +0700

    created project

---
+++ src/main/java/ru/qwerty/schedulerbot/SchedulerBotApplication.java
@@ -0,0 +1,13 @@
+package ru.qwerty.schedulerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerBotApplication.class, args);
+    }
+
+}
