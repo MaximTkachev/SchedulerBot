1ff9e3fc3f44bdce3e8cd54e1b53207eb35469ce
MaximTkachev <69761681+MaximTkachev@users.noreply.github.com>
2023-01-03 19:54:58 +0700

    added class for storing users in the db

---
+++ src/main/java/ru/qwerty/schedulerbot/entity/UserEntity.java
@@ -0,0 +1,31 @@
+package ru.qwerty.schedulerbot.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+/**
+ * User representation in the database.
+ */
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+
+    @Id
+    private Long id;
+
+    @Temporal(TemporalType.DATE)
+    private Date creationDate;
+}
