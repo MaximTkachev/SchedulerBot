530210be4744ff495fb89748d8fa468a8685a753
MaximTkachev <69761681+MaximTkachev@users.noreply.github.com>
2023-01-03 22:29:34 +0700

    implemented the command

--- src/main/java/ru/qwerty/schedulerbot/handler/implement/GetMenuHandler.java
+++ src/main/java/ru/qwerty/schedulerbot/handler/implement/GetMenuHandler.java
@@ -2,6 +2,7 @@ package ru.qwerty.schedulerbot.handler.implement;
 
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.qwerty.schedulerbot.handler.Handler;
+import ru.qwerty.schedulerbot.model.Command;
 
 /**
  * The handler is used for the case when a user wants to get a list of available bot commands.
@@ -10,6 +11,11 @@ public class GetMenuHandler implements Handler {
 
     @Override
     public String handle(Update update) {
-        return "Вы хотите получить меню";
+        return
+                Command.GET_CURRENT_GROUP + " получить вашу группу по умолчанию\n" +
+                Command.GET_SCHEDULE + " получить расписание вашей группы на сегодня\n" +
+                Command.SET_GROUP + " уставновить вашу группу по умолчанию\n" +
+                Command.SUBSCRIBE + " подписаться на ежедневную рассылку расписания\n" +
+                Command.UNSUBSCRIBE + " отписаться от ежедневной рассылки расписания";
     }
 }
