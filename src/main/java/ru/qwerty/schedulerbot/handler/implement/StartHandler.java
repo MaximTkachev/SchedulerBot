6f06fef67c024e19accadcdc2faa9e8f7d94dd50
MaximTkachev <69761681+MaximTkachev@users.noreply.github.com>
2023-01-03 19:58:22 +0700

    added command handling

--- src/main/java/ru/qwerty/schedulerbot/handler/implement/StartHandler.java
+++ src/main/java/ru/qwerty/schedulerbot/handler/implement/StartHandler.java
@@ -1,15 +1,40 @@
 package ru.qwerty.schedulerbot.handler.implement;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.qwerty.schedulerbot.converter.UserConverter;
+import ru.qwerty.schedulerbot.entity.UserEntity;
+import ru.qwerty.schedulerbot.exception.InternalException;
+import ru.qwerty.schedulerbot.mapper.UpdateMapper;
+import ru.qwerty.schedulerbot.model.Command;
 import ru.qwerty.schedulerbot.handler.Handler;
+import ru.qwerty.schedulerbot.service.UserService;
 
 /**
  * The handler is used to register user when they send the first message.
  */
+@Slf4j
+@RequiredArgsConstructor
 public class StartHandler implements Handler {
+
+    private final UserConverter converter;
+
+    private final UserService service;
 
     @Override
     public String handle(Update update) {
-        return "Вы хотите зарегистрироваться";
+        try {
+            UserEntity user = converter.convertToEntity(update);
+            service.save(user);
+            log.info("User was registered from update; {}", UpdateMapper.mapForLog(update));
+            return "Добро пожаловать!\nВведите " + Command.GET_MENU + " чтобы получить список доступных команд";
+        } catch (InternalException e) {
+            log.warn("Failed to register user from update: {}", UpdateMapper.mapForLog(update), e);
+            return e.getMessage();
+        } catch (Exception e) {
+            log.error("Failed to register user from update: {}", UpdateMapper.mapForLog(update), e);
+            return "Произошла непредвиденная ошибка на сервере";
+        }
     }
 }
