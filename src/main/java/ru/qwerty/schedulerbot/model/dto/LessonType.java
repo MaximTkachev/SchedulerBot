d70be679de2cff7e712c7859b5f80fe260a294ae
MaximTkachev <69761681+MaximTkachev@users.noreply.github.com>
2023-01-09 20:27:54 +0700

    added dto-s

---
+++ src/main/java/ru/qwerty/schedulerbot/model/dto/LessonType.java
@@ -0,0 +1,11 @@
+package ru.qwerty.schedulerbot.model.dto;
+
+public enum LessonType {
+    LECTURE,
+    SEMINAR,
+    PRACTICE,
+    LABORATORY,
+    DIFFERENTIAL_CREDIT,
+    CONSULTATION,
+    EXAM
+}
