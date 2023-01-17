4be81060af59884da3de1de6cb89553fd9a05c3b
MaximTkachev <69761681+MaximTkachev@users.noreply.github.com>
2023-01-17 11:50:14 +0700

    implemented command

--- src/main/java/ru/qwerty/schedulerbot/handler/implement/SubscribeHandler.java
+++ src/main/java/ru/qwerty/schedulerbot/handler/implement/SubscribeHandler.java
@@ -1,15 +1,25 @@
 package ru.qwerty.schedulerbot.handler.implement;
 
+import lombok.RequiredArgsConstructor;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.qwerty.schedulerbot.handler.Handler;
+import ru.qwerty.schedulerbot.model.UserChanges;
+import ru.qwerty.schedulerbot.service.UserService;
 
 /**
  * The handler is used for the case when a user wants to subscribe to a daily schedule mailing.
  */
+@RequiredArgsConstructor
 public class SubscribeHandler implements Handler {
+
+    private final UserService userService;
 
     @Override
     public String handle(Update update) {
-        return "Вы хотите подписаться на рассылку расписания";
+        UserChanges userChanges = new UserChanges();
+        userChanges.setIsSubscribed(true);
+        userService.update(update.getMessage().getChat().getId(), userChanges);
+
+        return "Вы успешно подписались на ежедневную рассылку расписания";
     }
 }
