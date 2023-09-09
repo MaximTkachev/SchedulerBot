3504b7a6c0b745524d97996f22849b2ad517103d
MaximTkachev <69761681+MaximTkachev@users.noreply.github.com>
2023-09-09 23:02:20 +0700

    refactoring

--- src/main/java/ru/qwerty/schedulerbot/client/TsuInTimeClient.java
+++ src/main/java/ru/qwerty/schedulerbot/client/TsuInTimeClient.java
@@ -1,4 +1,4 @@
-package ru.qwerty.schedulerbot.core.api;
+package ru.qwerty.schedulerbot.client;
 
 import ru.qwerty.schedulerbot.data.model.dto.Schedule;
 import ru.qwerty.schedulerbot.data.model.dto.Group;
